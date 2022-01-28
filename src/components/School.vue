04fb3cea1e21a304ed17cf45e4aa2b5bb862d894
酸性染料 <442444286@qq.com>
2022-01-28 19:59:57 +0800

    修改页面

---
+++ src/components/School.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2>学校名称：{{name}} </h2>
+        <h2>学校地址：{{address}} </h2>
+        <button v-on:click="toggle">更改名称</button>
+    </div>
+</template>
+
+<script>
+export default {
+    // 用于控制vue开发者工具中显示的名称
+    name:'School',
+    data() {
+        return {
+            name: '尚硅谷',
+            address: '北京',
+            switch: 0,
+        }
+    },
+    methods: {
+        toggle(){
+            this.name=this.switch==0?'atguigu':'尚硅谷';
+
+            this.switch=!this.switch;
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@
 import Vue from 'vue'
 // App组件时所有组件的父组件
 import App from './App.vue'
-
+// 关闭Vue的生产提示
 Vue.config.productionTip = false
 
+// 创建Vue实例对象vm
 new Vue({
+  // 将app组件放入容器中
   render: h => h(App),
 }).$mount('#app')
