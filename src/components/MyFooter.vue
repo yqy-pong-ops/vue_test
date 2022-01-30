80f7b54008afd19b49b8e335db135127b0c84728
酸性染料 <442444286@qq.com>
2022-01-30 15:17:53 +0800

    实现todoList案例

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,54 +1,45 @@
 <template>
-  <div>
-    <input type="checkbox" name="" id="" v-model="selectAll" v-on:change="changeAllSelected">
-    已完成{{finished}}/全部{{total}}
+  <div v-show="total">
+    <input type="checkbox" name="" id="" v-model="selectAll" />
+    已完成{{ finished }}/全部{{ total }}
     <button v-on:click="handleDelete">清除已完成</button>
   </div>
 </template>
 
 <script>
 export default {
-    name:'MyFooter',
-    props:['todos','changeAll','deleteDone'],
-    data() {
-      return {
-        selectAll:false,
-      }
+  name: "MyFooter",
+  props: ["todos", "changeAll", "deleteDone"],
+  computed: {
+    finished() {
+      let k = 0;
+      this.todos.forEach((element) => {
+        if (element.done) k++;
+      });
+      return k;
     },
-    computed: {
-      finished(){
-        let k=0;
-        this.todos.forEach(element => {
-          if(element.done) k++;
-        });
-        return k;
+    total() {
+      return this.todos.length;
+    },
+    selectAll: {
+      get() {
+        return this.finished == this.total && this.total;
       },
-      total(){
-        return this.todos.length;
-      }
-    },
-    methods: {
-      changeAllSelected(){
+      set(val) {
         // 通知APP进行changeAll
-        this.changeAll(this.selectAll);
+
+        this.changeAll(val);
       },
-      handleDelete(){
-        // 通知APP
-        this.deleteDone();
-      }
     },
-    watch: {
-      finished(val){
-        if(val==this.todos.length){
-          this.selectAll=true;
-        }else{
-          this.selectAll=false;
-        }
-      }
+  },
+  methods: {
+    handleDelete() {
+      // 通知APP
+      this.deleteDone();
     },
-}
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
