fd352c5a8a12a4045ce9eabe03b99ef8b888cfe6
酸性染料 <442444286@qq.com>
2022-01-30 23:37:46 +0800

    自定义事件

---
+++ src003-自定义事件/components/Student.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="demo" v-on:click="handleClick">
+      <h2>学生姓名：{{name}} </h2>
+      <h2>学生年龄：{{age}} </h2>
+      <h2>学生性别：{{sex}} </h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name:'张三',
+            age: 20,
+            sex: '男'
+        }
+    },
+    methods: {
+        handleClick(){
+            this.$emit('showName',this.name)
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .demo{
+        position: relative;
+        background-color: violet;
+        border: 1px solid;
+        margin: 10px;
+    }
+</style>
